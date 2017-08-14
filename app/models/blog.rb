9050854080012672378fc9954c129894b0efd615
John Doe <johndoe@example.com>
2017-08-14 09:57:58 +0000

    201708141901 start picture UPgit add -A

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,6 +2,5 @@ class Blog < ActiveRecord::Base
     validates:content,presence:true,uniqueness:true,
     length:{maximum:140}
     validates :image_url, presence: true
-    mount_uploader :image_url, ImageUploader 
 
 end
