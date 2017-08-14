20cfccb12bb6b31efba44146d920640459bb4c85
John Doe <johndoe@example.com>
2017-08-14 10:50:31 +0000

    201708141953 first succsess of up picturegit add -A

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170814100159) do
+ActiveRecord::Schema.define(version: 20170814101400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20170814100159) do
     t.datetime "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
   create_table "users", force: :cascade do |t|
